442fa047340c98e5814e4fcd4dc2c9daf6a19dbb
Hannah <hdvincelette@gmail.com>
2024-10-21 12:36:28 -0800

    Update clone.project.R

--- R/clone.project.R
+++ R/clone.project.R
@@ -3,14 +3,14 @@
 #' Creates a copy of a RDR project folder in a specified location. Remote users must be connected to one of the Service’s approved remote connection technologies, such as a Virtual Private Network (VPN).
 #' @param project Character string. Name of the project folder.
 #' @param local.path Character string. Directory path where the cloned project will be located. Default is the working directory, getwd().
-#' @param main Logical. Whether to return results from the "main" project folder (all subfolders except "incoming"). Default is TRUE.
+#' @param main Logical. Whether to return results from the main project subfolders (all subfolders except "incoming"). Default is TRUE.
 #' @param incoming Logical. Whether to return results from the "incoming" project subfolder. Default is TRUE.
 #' @return Returns a download of the project folder and all its contents.
 #' @keywords USFWS, repository, download, clone
 #' @seealso ```get.dir.template()```
 #' @export
 #' @examples
-#' # clone.project(project = "mbmlb_010_Grey_headed_chickadee_hybridization", path = getwd(), main = TRUE, incoming = TRUE)
+#' # clone.project(project = "mbmlb_010_Grey_headed_chickadee_hybridization", local.path = getwd(), main = TRUE, incoming = TRUE)
 
 
 clone.project <-
